a82aed6ee8f995ea6b5c6d90ef0dbc3aafd0a828
madidina <97095840+madidina@users.noreply.github.com>
2023-02-15 17:41:18 -0600

    Update Plotting.py
    add time and change Nan to 0

--- Plotting.py
+++ Plotting.py
@@ -7,6 +7,9 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import time
+
+t0 = time.clock()
 
 # Open
 merge_file = open('./MergedTime')
@@ -18,14 +21,13 @@ csv_merge_file = pd.read_csv(merge_file)
 csv_offer_file = pd.read_csv(offer_file)
 csv_bid_file = pd.read_csv(bid_file)
 
-# Init Arrays
+# Init Lists
 time_list = csv_merge_file.iloc[:, 0].tolist()
-bid_list = ['Nan']
-offer_list = ['Nan']
+bid_list = [0]
+offer_list = [0]
 spread_list = []
 
 # Import data 
-
 for index,row in csv_merge_file.iterrows():
     #time_list.append(row)
     if (row[1]=='b'):
@@ -39,6 +41,7 @@ for index,row in csv_merge_file.iterrows():
         offer_list.append(csv_offer_file.loc[row[2],:][1])
         spread_list.append(bid_list[-1] - offer_list[-1])
 
+print ('lists creat')
 #spread = [(o - b) for o, b in zip(offer_list, bid_list)]
 #spread = np.array(offer_list)-np.array(bid_list)
 
@@ -50,3 +53,5 @@ plt.show()
 plt.plot(time_list[:-3],spread_list, label='Spread')
 plt.legend()
 plt.show()
+
+print(t0)
