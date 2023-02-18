dd09c0e958756c41cec0ff321cc9eb90b5848e53
larissensium <123220119+larissensium@users.noreply.github.com>
2023-02-18 15:14:57 -0600

    Add files via upload
    I used all values in the Bid and offer files 
    I made some changes (i commented in the file) it doesn't give me a Key or "x, y must have the same dimension" Errors but I'm not sure if the graphs produced make sense ( the spread is still negative at the beginning)

---
+++ plotting.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 15 19:31:30 2023
+
+@author: laramatiukha
+"""
+
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
+
+t0 = time.clock()
+
+# Open
+merge_file = open('/Users/laramatiukha/Desktop/MergedTime')
+bid_file = open('/Users/laramatiukha/Desktop/bid_Price_File_3251758-2.csv')
+offer_file = open('/Users/laramatiukha/Desktop/offer_Price_File_3251758.csv')
+
+# Read csv: sep default ‘,’
+csv_merge_file = pd.read_csv(merge_file)
+csv_offer_file = pd.read_csv(offer_file)
+csv_bid_file = pd.read_csv(bid_file)
+
+# Init Lists
+time_list = csv_merge_file.iloc[:, 0].tolist()
+bid_list = [0]
+offer_list = [0]
+spread_list = []
+
+# Import data 
+for index,row in csv_merge_file.iterrows():
+    #time_list.append(row)
+    if (row[1]=='b'):
+        # Acces specific row and coloumn in a csv file
+        bid_list.append(csv_bid_file.loc[row[2],:][1])
+        offer_list.append(offer_list[-1])
+        spread_list.append(offer_list[-1] - bid_list[-1])
+        
+    else:
+        bid_list.append(bid_list[-1]) 
+        offer_list.append(csv_offer_file.loc[row[2],:][1])
+        spread_list.append(offer_list[-1] - bid_list[-1]) # since spread = offer - bid
+
+print ('lists creat')
+#spread = [(o - b) for o, b in zip(offer_list, bid_list)]
+#spread = np.array(offer_list)-np.array(bid_list)
+
+plt.plot(time_list, offer_list[:-1], label='Offer') # since len(time_list) < len(offer_list)
+plt.plot(time_list,bid_list[:-1], label='Bid') # len(time_list) < len(bid_list)
+plt.legend()
+plt.show()
+
+plt.plot(time_list,spread_list, label='Spread')
+plt.legend()
+plt.show()
+
+print(t0)
