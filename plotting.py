80e74bea7305d9761bff6582547a1a0ab663f178
madidina <97095840+madidina@users.noreply.github.com>
2023-03-02 21:03:50 -0600

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 15 19:31:30 2023
 
-@author: laramatiukha
-"""
+#Created on Mon Feb 13 09:04:10 2023
 
-
+#@author: madidina
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import time
-
-t0 = time.clock()
 
 # Open
-merge_file = open('/Users/laramatiukha/Desktop/MergedTime')
-bid_file = open('/Users/laramatiukha/Desktop/bid_Price_File_3251758-2.csv')
-offer_file = open('/Users/laramatiukha/Desktop/offer_Price_File_3251758.csv')
+merge_file = open('./MergedTimeBis')
+bid_file = open('./bid_Price_File_3251758.csv')
+offer_file = open('./offer_Price_File_3251758.csv')
 
 # Read csv: sep default ‘,’
 csv_merge_file = pd.read_csv(merge_file)
@@ -30,31 +24,47 @@ bid_list = [0]
 offer_list = [0]
 spread_list = []
 
+print('lists creation')
 # Import data 
 for index,row in csv_merge_file.iterrows():
-    #time_list.append(row)
+    
+    selected_row = row[2]
+    
+    if (selected_row == len(csv_bid_file)) or (selected_row == len(csv_offer_file)):
+        selected_row-=1
+ 
+    # Acces specific row and coloumn in a csv file
+    
     if (row[1]=='b'):
-        # Acces specific row and coloumn in a csv file
-        bid_list.append(csv_bid_file.loc[row[2],:][1]) # initially this line gave me a Key Error "382323" (which is also length of time_list
+        bid_list.append(csv_bid_file.loc[selected_row,:][1])
         offer_list.append(offer_list[-1])
         spread_list.append(offer_list[-1] - bid_list[-1])
         
+    elif (row[1]=='bo'):
+        bid_list.append(csv_bid_file.loc[selected_row,:][1])
+        offer_list.append(csv_offer_file.loc[row[3],:][1])
+        spread_list.append(offer_list[-1] - bid_list[-1])
+    
     else:
-        bid_list.append(bid_list[-1]) 
-        offer_list.append(csv_offer_file.loc[row[2],:][1])
-        spread_list.append(offer_list[-1] - bid_list[-1]) # since spread = offer - bid
+        bid_list.append(bid_list[-1])
+        offer_list.append(csv_offer_file.loc[selected_row,:][1])
+        spread_list.append(offer_list[-1] - bid_list[-1])
 
-print ('lists creat')
-#spread = [(o - b) for o, b in zip(offer_list, bid_list)]
-#spread = np.array(offer_list)-np.array(bid_list)
+    if (spread_list[-1]<0):
+        print(f' row: {row}\n index: {index}\n time: {time_list[index]}\n Bid: {bid_list[-1]}\n Offer: {offer_list[-1]}\n\n')
 
-plt.plot(time_list, offer_list[:-1], label='Offer') # since len(time_list) < len(offer_list)
-plt.plot(time_list,bid_list[:-1], label='Bid') # len(time_list) < len(bid_list)
+print ('lists created')
+
+plt.plot(time_list[1:],offer_list[1:-1], label='Offer')
+plt.plot(time_list[1:],bid_list[1:-1], label='Bid')
 plt.legend()
 plt.show()
 
-plt.plot(time_list,spread_list, label='Spread')
+plt.plot(time_list[1:],spread_list[1:], label='Spread')
 plt.legend()
 plt.show()
 
-print(t0)
+plt.hist(spread_list[1:])
+plt.show()
+
+# Info : 4 minutes of running time
